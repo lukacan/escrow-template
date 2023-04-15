995c3367360c2ea4a791033471e23d2d4f8dd847
lukacan <andrej.lukacovic98@gmail.com>
2023-04-15 16:34:51 +0200

    few tests added

--- tests/integration.rs
+++ tests/integration.rs
@@ -251,57 +251,38 @@ fn vote_negative(
 
     rpc_client.send_and_confirm_transaction(&transaction)
 }
-#[test]
-fn test_basic() {
-    solana_logger::setup_with_default("solana_program_runtime=debug");
 
-    let mut testvalgen = TestValidatorGenesis::default();
-
-    let initializer = Keypair::new();
-
-    let account = AccountSharedData::new(
-        LAMPORTS_PER_SOL * 2,
-        0,
-        &solana_program::system_program::id(),
-    );
-    testvalgen.add_account(initializer.pubkey(), account);
-
+fn add_account(testvalgen: &mut TestValidatorGenesis) -> Keypair {
     let alice = Keypair::new();
-
     let account = AccountSharedData::new(
         LAMPORTS_PER_SOL * 2,
         0,
         &solana_program::system_program::id(),
     );
     testvalgen.add_account(alice.pubkey(), account);
+    return alice;
+}
 
-    let bob = Keypair::new();
+#[test]
+fn test_basic() {
+    // solana_logger::setup_with_default("solana_program_runtime=debug");
+    // solana_logger::setup_with_default("solana_runtime::message=debug");
 
-    let account = AccountSharedData::new(
-        LAMPORTS_PER_SOL * 2,
-        0,
-        &solana_program::system_program::id(),
-    );
-    testvalgen.add_account(bob.pubkey(), account);
+    let mut testvalgen = TestValidatorGenesis::default();
 
-    let diana = Keypair::new();
-
-    let account = AccountSharedData::new(
-        LAMPORTS_PER_SOL * 2,
-        0,
-        &solana_program::system_program::id(),
-    );
-    testvalgen.add_account(diana.pubkey(), account);
+    let initializer = add_account(&mut testvalgen);
+    let alice = add_account(&mut testvalgen);
+    let bob = add_account(&mut testvalgen);
+    let diana = add_account(&mut testvalgen);
 
     let (test_validator, _payer) = testvalgen
         .add_program("target/deploy/bpf_program_template", id())
         .start();
-
-    solana_logger::setup_with_default("solana_runtime::message=debug");
     let rpc_client = test_validator.get_rpc_client();
 
     let alice_party: String = String::from("Alice Party");
     let diana_party: String = String::from("Diana Party");
+
 
     assert_matches!(initialize(&rpc_client, &initializer), Ok(_));
 
@@ -310,7 +291,7 @@ fn test_basic() {
         Ok(_)
     );
     assert_matches!(
-        create_party(&rpc_client, &initializer, &alice, &diana_party),
+        create_party(&rpc_client, &initializer, &diana, &diana_party),
         Ok(_)
     );
     assert_matches!(
@@ -341,4 +322,198 @@ fn test_basic() {
         vote_negative(&rpc_client, &initializer.pubkey(), &bob, &alice_party),
         Err(_)
     );
+}
+
+#[test]
+fn happy_path1() {
+    // solana_logger::setup_with_default("solana_program_runtime=debug");
+    // solana_logger::setup_with_default("solana_runtime::message=debug");
+
+    let mut testvalgen = TestValidatorGenesis::default();
+
+    let initializer = add_account(&mut testvalgen);
+    let alice = add_account(&mut testvalgen);
+    let bob = add_account(&mut testvalgen);
+    let diana = add_account(&mut testvalgen);
+    let michael = add_account(&mut testvalgen);
+
+    let alice_party: String = String::from("Alice Party");
+    let diana_party: String = String::from("Diana Party");
+    let michael_party: String = String::from("Michael Party");
+
+    let (test_validator, _payer) = testvalgen
+        .add_program("target/deploy/bpf_program_template", id())
+        .start();
+
+    let rpc_client = test_validator.get_rpc_client();
+
+    assert_matches!(initialize(&rpc_client, &initializer), Ok(_));
+
+    assert_matches!(
+        create_party(&rpc_client, &initializer, &alice, &alice_party),
+        Ok(_)
+    );
+    assert_matches!(
+        create_party(&rpc_client, &initializer, &michael, &michael_party),
+        Ok(_)
+    );
+    assert_matches!(
+        create_party(&rpc_client, &initializer, &diana, &diana_party),
+        Ok(_)
+    );
+    assert_matches!(
+        create_voter(&rpc_client, &initializer.pubkey(), &bob),
+        Ok(_)
+    );
+    assert_matches!(
+        vote_positive(&rpc_client, &initializer.pubkey(), &bob, &alice_party),
+        Ok(_)
+    );
+    assert_matches!(
+        vote_positive(&rpc_client, &initializer.pubkey(), &bob, &michael_party),
+        Ok(_)
+    );
+    assert_matches!(
+        vote_negative(&rpc_client, &initializer.pubkey(), &bob, &diana_party),
+        Ok(_)
+    );
+    assert_matches!(
+        vote_positive(&rpc_client, &initializer.pubkey(), &bob, &diana_party),
+        Err(_)
+    );
+    assert_matches!(
+        vote_negative(&rpc_client, &initializer.pubkey(), &bob, &alice_party),
+        Err(_)
+    );
+}
+
+#[test]
+fn happy_path2(){
+
+    // solana_logger::setup_with_default("solana_program_runtime=debug");
+    // solana_logger::setup_with_default("solana_runtime::message=debug");
+
+    let mut testvalgen = TestValidatorGenesis::default();
+
+    let initializer = add_account(&mut testvalgen);
+    let alice = add_account(&mut testvalgen);
+    let bob = add_account(&mut testvalgen);
+
+    let alice_party: String = String::from("Alice Party");
+
+    let (test_validator, _payer) = testvalgen
+        .add_program("target/deploy/bpf_program_template", id())
+        .start();
+
+    let rpc_client = test_validator.get_rpc_client();
+
+    assert_matches!(initialize(&rpc_client, &initializer), Ok(_));
+
+    assert_matches!(
+        create_party(&rpc_client, &initializer, &alice, &alice_party),
+        Ok(_)
+    );
+    assert_matches!(
+        create_voter(&rpc_client, &initializer.pubkey(), &bob),
+        Ok(_)
+    );
+    assert_matches!(
+        vote_negative(&rpc_client, &initializer.pubkey(), &bob, &alice_party),
+        Err(_)
+    );
+}
+
+#[test]
+fn happy_path3(){    
+    // solana_logger::setup_with_default("solana_program_runtime=debug");
+    // solana_logger::setup_with_default("solana_runtime::message=debug");
+
+    let mut testvalgen = TestValidatorGenesis::default();
+
+    let initializer = add_account(&mut testvalgen);
+    let alice = add_account(&mut testvalgen);
+
+    let (test_validator, _payer) = testvalgen
+        .add_program("target/deploy/bpf_program_template", id())
+        .start();
+
+    let rpc_client = test_validator.get_rpc_client();
+
+    let blockhash = rpc_client.get_latest_blockhash().unwrap();
+
+    let (pda_owner, bump_owner) =
+        Pubkey::find_program_address(&[b"voting_owner", initializer.pubkey().as_ref()], &id());
+
+    let (pda_state, bump_state) =
+        Pubkey::find_program_address(&[b"voting_state", pda_owner.as_ref()], &id());
+
+    let instruction_data = vec![0u8, bump_owner, bump_state];
+
+    let mut transaction = Transaction::new_with_payer(
+        &[Instruction {
+            program_id: id(),
+            accounts: vec![
+                AccountMeta::new(initializer.pubkey(), false), // initializer
+                AccountMeta::new(pda_owner, false),           // voting owner
+                AccountMeta::new(pda_state, false),           // voting state
+                AccountMeta::new_readonly(solana_program::system_program::id(), false),
+                AccountMeta::new(alice.pubkey(), true),
+            ],
+            data: instruction_data,
+        }],
+        Some(&alice.pubkey()),
+    );
+    transaction.sign(&[&alice], blockhash);
+
+    assert_matches!(rpc_client.send_and_confirm_transaction(&transaction),Err(_));
+
+}
+
+#[test]
+fn happy_path4(){    
+    let mut testvalgen = TestValidatorGenesis::default();
+
+    let initializer = add_account(&mut testvalgen);
+    let voter = add_account(&mut testvalgen);
+
+    let (test_validator, _payer) = testvalgen
+        .add_program("target/deploy/bpf_program_template", id())
+        .start();
+
+    let rpc_client = test_validator.get_rpc_client();
+
+    let blockhash = rpc_client.get_latest_blockhash().unwrap();
+
+    let (pda_owner, bump_owner) =
+        Pubkey::find_program_address(&[b"voting_owner", initializer.pubkey().as_ref()], &id());
+
+    let (pda_state, bump_state) =
+        Pubkey::find_program_address(&[b"voting_state", pda_owner.as_ref()], &id());
+
+    let (pda_voter, bump_voter) = Pubkey::find_program_address(
+        &[b"new_voter", voter.pubkey().as_ref(), pda_state.as_ref()],
+        &id(),
+    );
+
+    let instruction_data = vec![2u8, bump_owner, bump_state, bump_voter];
+
+    let mut transaction = Transaction::new_with_payer(
+        &[Instruction {
+            program_id: id(),
+            accounts: vec![
+                AccountMeta::new(voter.pubkey(), true), // persone that wants to be voter
+                AccountMeta::new_readonly(initializer.pubkey(), false), // owner
+                AccountMeta::new_readonly(pda_owner, false), // voting owner
+                AccountMeta::new_readonly(pda_state, false), // voting state
+                AccountMeta::new(pda_voter, false),     // voter
+                AccountMeta::new_readonly(solana_program::system_program::id(), false),
+            ],
+            data: instruction_data,
+        }],
+        Some(&voter.pubkey()),
+    );
+    transaction.sign(&[&voter], blockhash);
+
+    assert_matches!(rpc_client.send_and_confirm_transaction(&transaction),Err(_));
+
 }
